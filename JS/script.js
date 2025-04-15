a6c8eabd253e4bbcb2c8585c7bd655fe295fd402
Codeby-Ryan <Ryanrodrigues9890@gmail.com>
2025-04-15 16:14:47 -0300

    Atualização de arquivos

---
+++ JS/script.js
@@ -0,0 +1,31 @@
+// SETAS PARA MUDAR OS ITENS DO MEU MENU-CARDÁPIO
+new Swiper(".grid-content", {
+  loop: true,
+  spaceBetween: 30,
+
+  // Pagination bullets
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  //RESPONSIVIDADE DOS MEUS BREACKPOINTS
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 1,
+    },
+    1024: {
+      slidesPerView: 1,
+    },
+  },
+});
